cfa7c1016ea08b1f25139d47b23040a2a38997d4
Husky Lover <ssolivares0@gmail.com>
2025-06-29 22:24:37 -0400

    Fixed errors in UserRepository and PostRepository interfaces

--- tech-news-java-api/src/main/java/com/technews/repository/PostRepository.java
+++ tech-news-java-api/src/main/java/com/technews/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.technews.repository;
 
 import com.technews.model.Post;
+import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post,Integer> {
-    List<Post> findAllByUserId(Integer id) throws Exception;
+    List<Post> findAllPostsByUserId(Integer id) throws Exception;
 }
